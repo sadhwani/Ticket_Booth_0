7b241050b326c1451efb2e62a60e0636d6ae0961
sadhwani <sadhwani@mail.usf.edu>
2016-04-20 23:09:36 -0400

    Update CommandProcessor.cpp

--- CommandProcessor.cpp
+++ CommandProcessor.cpp
@@ -1,96 +1,142 @@
+#include <iostream>
 #include "CommandProcessor.h"
-
-CommandProcessor::CommandProcessor(TiXmlNode* venue_file_node) : venue_file_node(venue_file_node)
-{}
-
+#include "Venue.h"
 using namespace std;
 
-void CommandProcessor::firstLevel(TiXmlNode* venue_file_node, Venue** venues) {
-    int level;
-    cout << "Enter command number" << endl;
-    cout << "1: Select Venue" << endl;
-    cout << "2: Quit" << endl;
-    cin >> level;
-    if(level == 1) {
-        TiXmlNode* venue_node = venue_file_node->FirstChild();
-        assert(venue_node != 0);
-        //cout << venue_node->Value() << endl;
-        int i = 0;
-        while(venue_node!= 0) {
-            venues[i] = Venue_from_Xml::Get_Venue(venue_node);
-            venue_node = venue_node->NextSibling();
-            //venues[i]->Display();
-            i++;
-        }
-        secondLevel(venues, i);
-    } else if(level == 2) {
-        quit();
-    } else {
-        cout << "Integer not recognized as a command. Quitting" << endl;
-    }
-}
+Command_States CommandProcessor:: command_state = Initial;
 
-void CommandProcessor::secondLevel(Venue** venues, int numOfVenues) {
-    cout << "Second level" << endl;
-    cout << "Select Venue: " << endl;
-    int choice;
-    
-    int i;
-    for(i = 0; i < numOfVenues; i++) {
-        cout << (i+1) << ": ";
-        venues[i]->Display();
-    }
-    //cout << "i is: " << i << " after for loop in secondLevel" << endl;
-    cout << (i+1) << ": Cancel" << endl;
-    cin >> choice;
-    if(choice == (numOfVenues+1)) {
-        // Implement cancel feature here
-    } else {
-        venueOptions(venues, choice);
-    }
-}
+Menu* CommandProcessor::menus[NR_CMD_STATES];
+Venue** CommandProcessor::venues = 0;
+int CommandProcessor::nr_venues = 0;
+Venue* CommandProcessor::selected_venue = 0;
 
-void CommandProcessor::venueOptions(Venue** venues, int venueNumber) {
-    int choice;
-    
-    cout << "Selected venue is ";
-    venues[venueNumber-1]->Display();
-    cout << endl;
-    
-    cout << "Enter command number: " << endl;
-    cout << "1: Display Venue" << endl;
-    cout << "2: Select Show" << endl;
-    cout << "3: Change Venue" << endl;
-    cout << "4: Quit" << endl;
-    
-    cin >> choice;
-    switch(choice) {
-        case 1:
-            // Display 1 Venue
-            venues[venueNumber-1]->Display_All();
-            break;
-        case 2:
-            // Supposed to select show here, add implementation later
-            cout << "Selecting Show..." << endl;
-            break;
-        case 3:
-            // Change Venue
-            secondLevel(venues, venueNumber);
-            break;
-        case 4:
-            // Quit
-            quit();
-            break;
-        default:
-            cout << "Wrong number picked. Quitting.";
-            quit();
-    }
-}
 
-int CommandProcessor::quit() {
-    cout << "Quitting " << endl;
-    // returning random number to terminate the program
-    return 3;
-    
-    // Add program termination feature here
+void CommandProcessor::Process_Commands(Venue **venues_, int num_venues_)
+{
+	cout << "Process commands starting\n";
+
+	venues  = venues_;
+	nr_venues = num_venues_;
+
+	Create_Menus(venues,num_venues_);
+	
+	while (command_state != Done)
+	{
+		if (command_state == Venue_Option)
+		{
+			cout << "Selected venue is "
+				<< selected_venue->Name() << endl;
+		}
+		const string* cmd = menus[command_state]->Get_Command();
+
+		switch (command_state)
+		{
+		case Initial:   Process_Initial_Cmd(*cmd);
+			break;
+
+		case Venue_Selected:    Process_Venue_Selected_Cmd(*cmd);
+			break;
+
+		case Venue_Option: Process_Venue_Selected_Cmd(*cmd);
+
+		case Done:  break;      // Can't happen
+		}
+	}
+
+	cout << "Command_Processor exiting\n";
+	// Process command in Initial command state
+
+}
+	void CommandProcessor::Process_Initial_Cmd(const string& cmd)
+	{
+		if (cmd == "Select Venue")
+		{
+			Select_Venue();
+			//command_state = Venue_Option;
+			command_state = Venue_Selected;
+		}
+		
+		else
+		{
+			cout << "Quit command\n";
+			command_state = Done;
+		}
+	}
+
+	// Process command in State Selected command state
+	void CommandProcessor::Process_Venue_Selected_Cmd(const string& cmd)
+	{
+		if (cmd == "Display Venue")
+		{
+			selected_venue->Display_All();
+		}
+		else if (cmd == "Change Venue")
+		{
+			
+			command_state = Initial;
+		}
+
+		else if (cmd == "Select Show")
+		{
+			cout << "Select_Show called\n";
+		}
+		else
+		{
+			cout << "Quit command\n";
+			command_state = Done;
+		}
+	}
+
+	 void CommandProcessor::Select_Venue()
+	{
+		Menu* menu = new Menu("Select Venue");
+		
+		for (int i = 0; i < nr_venues; ++i)
+		{
+			menu->Add_Command(venues[i]->Name());
+			
+		}
+
+		const string* venue_name = menu->Get_Command();
+
+		// Find Venue with this name
+		for (int i = 0; i < nr_venues; ++i)
+		{
+			if (venues[i]->Name() == *venue_name)
+			{
+				selected_venue = venues[i];
+				//command_state = Venue_Selected; 
+				command_state = Venue_Option;
+				return;
+			}
+		}
+		// Should never reach this point.
+		cout << "Error in Command_Processor::Select_State()\n";
+	}
+
+
+void CommandProcessor::Create_Menus(Venue **venues, int num_venues)
+{
+	// Menu for Initial command state
+	Menu* menu = new Menu("Enter command number");
+	menu->Add_Command("Select Venue");
+	menu->Add_Command("Quit");
+	menus[Initial] = menu;
+
+	// Menu for Venue Selected
+	menu = new Menu("Select Venue");
+	
+	for (int i = 0; i < num_venues; i++)
+	{
+		menu->Add_Command(venues[i]->Name());
+	}
+	menu->Add_Command("Cancel");
+	menus[Venue_Option] = menu;
+
+	menu = new Menu("Enter command number");
+	menu->Add_Command("Display Venue");
+	menu->Add_Command("Select Show");
+	menu->Add_Command("Change Venue");
+	menu->Add_Command("Quit");
+	menus[Venue_Selected] = menu;
 }
