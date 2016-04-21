6c44e3e3a13a9e9d5fc8d3f4db26482e418dd470
sadhwani <sadhwani@mail.usf.edu>
2016-04-20 23:10:14 -0400

    Update CommandProcessor.h

--- CommandProcessor.h
+++ CommandProcessor.h
@@ -1,4 +1,4 @@
-#pragma once
+/*#pragma once
 #include <iostream>
 #include "Venue_from_Xml.h"
 
@@ -11,5 +11,36 @@ public:
     void secondLevel(Venue** venues, int numOfVenues);
     void venueOptions(Venue** venues, int venueNumber);
     int quit();
+};
+*/
 
+#pragma once
+#include <string>
+#include "Menu.h"
+#include "Venue.h"
+enum Command_States { Initial, Venue_Option, Venue_Selected,  Done };
+using namespace std;
+
+class CommandProcessor
+{
+public:
+	static void Process_Commands(Venue **venues, int num_venues);
+
+private:
+	static const int NR_CMD_STATES = (int)Done + 1;
+	static Command_States command_state;
+	static Menu* menus[NR_CMD_STATES];
+
+	static void Create_Menus(Venue **venues, int num_venues);
+	static void Process_Initial_Cmd(const string& cmd);
+	static void Process_Venue_Selected_Cmd(const string& cmd);
+
+	static void Select_Venue();
+	static Venue** venues;
+	static int nr_venues;
+	static Venue* selected_venue;
+
+
+
+	CommandProcessor(void) {};
 };
